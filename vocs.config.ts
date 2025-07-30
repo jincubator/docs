e276ba2b4dd219ee4e320f96405917950a4d680a
johnwhitton <john@johnwhitton.com>
2025-07-29 23:46:10 -0700

    Migration WIP

--- vocs.config.ts
+++ vocs.config.ts
@@ -5,7 +5,7 @@ import rehypeMathjax from "rehype-mathjax";
 export default defineConfig({
   title: "Jincubator",
   description:
-    "Jincubator is a protocol for creating and managing intent swaps.",
+    "Research focused on Intent BasedSolving, Arbitrage and Market Making",
   iconUrl: "/images/jincubator.png",
   logoUrl: "/images/jincubator.png",
   ogImageUrl:
@@ -16,73 +16,53 @@ export default defineConfig({
   },
   topNav: [
     {
-      text: "Home",
-      link: "/",
-    },
-    {
-      text: "Writing",
-      link: "/writing/intro",
-    },
-    {
-      text: "Research",
-      link: "/research/intro",
-    },
-  ],
-  sidebar: [
-    {
-      text: "Overview",
-      link: "/overview",
-    },
-    {
-      text: "Writing",
-      link: "/writing/intro",
-    },
-    {
       text: "Research",
       link: "/research/intro",
     },
     {
-      text: "Design",
-      collapsed: false,
-      items: [
-        { text: "Design Intro", link: "/design/intro" },
-        { text: "Architecture", link: "/design/architecture" },
-        { text: "Protocol", link: "/design/protocol" },
-        { text: "Resource Management", link: "/design/resources" },
-        { text: "IntentSwap Flow", link: "/design/flow" },
-        { text: "Hook Design - WIP", link: "/design/hooks" },
-      ],
+      text: "Products",
+      link: "/products/intro",
     },
     {
-      text: "Writing",
-      link: "/writing/intro",
-      items: [
-        { text: "John's Writing", link: "/writing/intro" },
-        {
-          text: "Ethereum Bridging using Light Clients - Rainbow Costing",
-          link: "/writing/2023-02-23-rainbow-costs",
-        },
-        {
-          text: "Draft EAVE Whitepaper",
-          link: "/writing/2021-12-01-eave-defi",
-        },
-        {
-          text: "Kanga Protocol Whitepaper",
-          link: "/writing/2021-05-01-kanga",
-        },
-        {
-          text: "EAVE Parachain Design",
-          link: "/writing/2021-04-01-eave-parachain",
-        },
-        {
-          text: "Lessons Learned from Devcon5",
-          link: "/writing/2019-10-16-devcon5",
-        },
-        {
-          text: "Project-X Technical White Paper",
-          link: "/writing/2018-07-17-projectx",
-        },
-      ],
+      text: "Partnerships",
+      link: "/partnerships/intro",
     },
   ],
+  sidebar: {
+    "/products": [
+      { text: "Products", link: "/products/intro" },
+      {
+        text: "Ethereum Bridging using Light Clients - Rainbow Costing",
+        link: "/products/2023-02-23-rainbow-costs",
+      },
+      { text: "Draft EAVE Whitepaper", link: "/products/2021-12-01-eave-defi" },
+      { text: "Kanga Protocol Whitepaper", link: "/products/2021-05-01-kanga" },
+      {
+        text: "EAVE Parachain Design",
+        link: "/products/2021-04-01-eave-parachain",
+      },
+      {
+        text: "Lessons Learned from Devcon5",
+        link: "/products/2019-10-16-devcon5",
+      },
+      {
+        text: "Project-X Technical White Paper",
+        link: "/products/2018-07-17-projectx",
+      },
+    ],
+    "/research": [
+      {
+        text: "Research",
+        collapsed: false,
+        link: "/research/intro",
+      },
+      { text: "Design Intro", link: "/design/intro" },
+      { text: "Architecture", link: "/design/architecture" },
+      { text: "Protocol", link: "/design/protocol" },
+      { text: "Resource Management", link: "/design/resources" },
+      { text: "IntentSwap Flow", link: "/design/flow" },
+      { text: "Hook Design - WIP", link: "/design/hooks" },
+    ],
+    "/partnerships": [{ text: "Partnerships", link: "/partnerships/intro" }],
+  },
 });
