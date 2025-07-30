97af8a076195621b88ffc163eb84c6fa11bdec59
John Whitton <john@johnwhitton.com>
2025-07-29 22:00:58 -0700

    Initial migration WIP

--- vocs.config.ts
+++ vocs.config.ts
@@ -10,10 +10,36 @@ export default defineConfig({
   logoUrl: "/images/jincubator.png",
   ogImageUrl:
     "https://vocs.dev/api/og?logo=%logo&title=%title&description=%description",
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeMathjax],
+  },
+  topNav: [
+    {
+      text: "Home",
+      link: "/",
+    },
+    {
+      text: "Writing",
+      link: "/writing/intro",
+    },
+    {
+      text: "Research",
+      link: "/research/intro",
+    },
+  ],
   sidebar: [
     {
       text: "Overview",
       link: "/overview",
+    },
+    {
+      text: "Writing",
+      link: "/writing/intro",
+    },
+    {
+      text: "Research",
+      link: "/research/intro",
     },
     {
       text: "Design",
@@ -27,9 +53,36 @@ export default defineConfig({
         { text: "Hook Design - WIP", link: "/design/hooks" },
       ],
     },
+    {
+      text: "Writing",
+      link: "/writing/intro",
+      items: [
+        { text: "John's Writing", link: "/writing/intro" },
+        {
+          text: "Ethereum Bridging using Light Clients - Rainbow Costing",
+          link: "/writing/2023-02-23-rainbow-costs",
+        },
+        {
+          text: "Draft EAVE Whitepaper",
+          link: "/writing/2021-12-01-eave-defi",
+        },
+        {
+          text: "Kanga Protocol Whitepaper",
+          link: "/writing/2021-05-01-kanga",
+        },
+        {
+          text: "EAVE Parachain Design",
+          link: "/writing/2021-04-01-eave-parachain",
+        },
+        {
+          text: "Lessons Learned from Devcon5",
+          link: "/writing/2019-10-16-devcon5",
+        },
+        {
+          text: "Project-X Technical White Paper",
+          link: "/writing/2018-07-17-projectx",
+        },
+      ],
+    },
   ],
-  markdown: {
-    remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeMathjax],
-  },
 });
