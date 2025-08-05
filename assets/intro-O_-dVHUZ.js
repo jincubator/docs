9639d1899bd4097bef25cc99dba8dab99c1e65d4
johnwhitton <john@johnwhitton.com>
2025-08-05 16:51:05 -0700

    Updates

---
+++ assets/intro-O_-dVHUZ.js
@@ -0,0 +1,6 @@
+import{f as s,j as e}from"./index-CsmQaVFF.js";const o=void 0;function t(n){const r={a:"a",p:"p",...s(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsxs("div",{align:"center",children:[e.jsx("h1",{align:"center",children:"Product Incubations"}),e.jsx("p",{align:"center",children:e.jsx(r.p,{children:"Various Product papers on Parachains, Bridges, DEX and Infrastructure."})})]}),`
+`,e.jsxs(r.p,{children:[`The articles on this site, relates to the research done and products built with varying degrees of success.
+Some won prizes in hackathons, others had seed funding commitments but did not launch.
+The majority of the research and design here lead to permanent roles with organizations building in the same space.
+For deeper research on solving, arbitrage, bridging, zero knowledge and chain consensus and signing see `,e.jsx(r.a,{href:"/research/intro",children:"research"}),`.
+This section is where that research was brought together to form an idea of a platform.`]})]})}function i(n={}){const{wrapper:r}={...s(),...n.components};return r?e.jsx(r,{...n,children:e.jsx(t,{...n})}):t(n)}export{i as default,o as frontmatter};
