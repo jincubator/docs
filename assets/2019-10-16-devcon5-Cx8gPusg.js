a10aa38ad4558ae35b78ddbcfa20677a4d23a09c
johnwhitton <john@johnwhitton.com>
2026-08-16 17:06:00 -0700

    Updates

---
+++ assets/2019-10-16-devcon5-Cx8gPusg.js
@@ -0,0 +1,38 @@
+import{u as s,j as e}from"./index-VjbClsXE.js";const t={title:"Lessons Learned from Devcon5",description:"undefined"};function i(n){const a={a:"a",blockquote:"blockquote",button:"button",div:"div",h1:"h1",h2:"h2",header:"header",li:"li",ol:"ol",p:"p",span:"span",strong:"strong",ul:"ul",...s(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(a.blockquote,{"data-historical-context":"true",children:e.jsxs(a.p,{children:["Historical page retained for compatibility. It may contain superseded assumptions, incomplete plans, or outdated links and is not current canonical content. See the ",e.jsx(a.a,{href:"/archive",children:"Archive"})," for context."]})}),`
+`,e.jsx(a.header,{children:e.jsxs(a.h1,{id:"lessons-learned-from-devcon5",children:["Lessons Learned from Devcon5",e.jsx(a.a,{"aria-hidden":"true",tabIndex:"-1",href:"#lessons-learned-from-devcon5",children:e.jsx(a.div,{"data-autolink-icon":!0})})]})}),`
+`,e.jsx(a.p,{children:"Devcon5 key take aways on emerging blockchain technologies, new initatives and what the community is looking for."}),`
+`,e.jsxs(a.ul,{children:[`
+`,e.jsx(a.li,{children:"date: 2019-10-16"}),`
+`,e.jsx(a.li,{children:"author: John Whitton"}),`
+`]}),`
+`,e.jsxs(a.h2,{id:"introduction",children:["Introduction",e.jsx(a.a,{"aria-hidden":"true",tabIndex:"-1",href:"#introduction",children:e.jsx(a.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsx(a.p,{children:"Having just returned from Devcon5 I’d like to take a moment to thank Tokyo and Osaka specifically for the warmth and hospitality shown. Secondly I’d like to congratulate the Ethereum community for its vibrant and inclusive culture. The community continues to get stronger every year and the level of research and innovation is amazing."}),`
+`,e.jsx(a.p,{children:"So with that being said, I thought I’d also share some of the lessons that the Harmony team learned and also some focus areas for us as we move forward. In the form of a few slides."}),`
+`,e.jsx("span",{className:"image main",children:e.jsxs(a.button,{type:"button",className:"jincubator-image-zoom","aria-label":"Open full-size image: Harmony team at Devcon 5",children:[e.jsx("img",{src:"/posts/2019-10-16-devcon5/1_meet.webp",alt:"Harmony team at Devcon 5",className:"jincubator-image-zoom__image"}),e.jsx(a.span,{className:"jincubator-image-zoom__indicator","aria-hidden":"true"})]})}),`
+`,e.jsx(a.p,{children:"The highlight as you can see below is engaging and learning from the quality researchers and partners at the conference."}),`
+`,e.jsx("span",{className:"image main",children:e.jsxs(a.button,{type:"button",className:"jincubator-image-zoom","aria-label":"Open full-size image: Devcon 5 research and partner highlights",children:[e.jsx("img",{src:"/posts/2019-10-16-devcon5/2_highlight.webp",alt:"Devcon 5 research and partner highlights",className:"jincubator-image-zoom__image"}),e.jsx(a.span,{className:"jincubator-image-zoom__indicator","aria-hidden":"true"})]})}),`
+`,e.jsx(a.p,{children:"The following 5 areas we found of most interest to help us better serve our developer and user community."}),`
+`,e.jsx("span",{className:"image main",children:e.jsxs(a.button,{type:"button",className:"jincubator-image-zoom","aria-label":"Open full-size image: Five lessons from Devcon 5",children:[e.jsx("img",{src:"/posts/2019-10-16-devcon5/3_lessons.webp",alt:"Five lessons from Devcon 5",className:"jincubator-image-zoom__image"}),e.jsx(a.span,{className:"jincubator-image-zoom__indicator","aria-hidden":"true"})]})}),`
+`,e.jsxs(a.ol,{children:[`
+`,e.jsx(a.li,{children:"As a layer one solution scaling is of keen interest to us and it was exciting to see the amount of work done on Layer II solutions."}),`
+`]}),`
+`,e.jsx("span",{className:"image main",children:e.jsxs(a.button,{type:"button",className:"jincubator-image-zoom","aria-label":"Open full-size image: Layer 2 scaling approaches discussed at Devcon 5",children:[e.jsx("img",{src:"/posts/2019-10-16-devcon5/4_layer2.webp",alt:"Layer 2 scaling approaches discussed at Devcon 5",className:"jincubator-image-zoom__image"}),e.jsx(a.span,{className:"jincubator-image-zoom__indicator","aria-hidden":"true"})]})}),`
+`,e.jsxs(a.ol,{start:"2",children:[`
+`,e.jsx(a.li,{children:"The overwhelming message from the community is that wallets need to be simplified to drive Main stream adoption."}),`
+`]}),`
+`,e.jsx("span",{className:"image main",children:e.jsxs(a.button,{type:"button",className:"jincubator-image-zoom","aria-label":"Open full-size image: Wallet usability and mainstream adoption",children:[e.jsx("img",{src:"/posts/2019-10-16-devcon5/5_wallets.webp",alt:"Wallet usability and mainstream adoption",className:"jincubator-image-zoom__image"}),e.jsx(a.span,{className:"jincubator-image-zoom__indicator","aria-hidden":"true"})]})}),`
+`,e.jsxs(a.ol,{start:"3",children:[`
+`,e.jsx(a.li,{children:"Bridging decentralized finance and traditional finance continues to be sought after."}),`
+`]}),`
+`,e.jsx("span",{className:"image main",children:e.jsxs(a.button,{type:"button",className:"jincubator-image-zoom","aria-label":"Open full-size image: Connecting decentralized and traditional finance",children:[e.jsx("img",{src:"/posts/2019-10-16-devcon5/6_tokens.webp",alt:"Connecting decentralized and traditional finance",className:"jincubator-image-zoom__image"}),e.jsx(a.span,{className:"jincubator-image-zoom__indicator","aria-hidden":"true"})]})}),`
+`,e.jsxs(a.ol,{start:"4",children:[`
+`,e.jsx(a.li,{children:"Currently blockchains are still limited by scalability and settlement time issues."}),`
+`]}),`
+`,e.jsx("span",{className:"image main",children:e.jsxs(a.button,{type:"button",className:"jincubator-image-zoom","aria-label":"Open full-size image: Blockchain scalability and settlement challenges",children:[e.jsx("img",{src:"/posts/2019-10-16-devcon5/7_settlement.webp",alt:"Blockchain scalability and settlement challenges",className:"jincubator-image-zoom__image"}),e.jsx(a.span,{className:"jincubator-image-zoom__indicator","aria-hidden":"true"})]})}),`
+`,e.jsxs(a.ol,{start:"5",children:[`
+`,e.jsx(a.li,{children:"Sharding is one of the key approaches for scalability. It was refreshing to see some of the innovative methods people are using for this."}),`
+`]}),`
+`,e.jsx("span",{className:"image main",children:e.jsxs(a.button,{type:"button",className:"jincubator-image-zoom","aria-label":"Open full-size image: Sharding approaches for blockchain scalability",children:[e.jsx("img",{src:"/posts/2019-10-16-devcon5/8_sharding.webp",alt:"Sharding approaches for blockchain scalability",className:"jincubator-image-zoom__image"}),e.jsx(a.span,{className:"jincubator-image-zoom__indicator","aria-hidden":"true"})]})}),`
+`,e.jsxs(a.p,{children:["I hope you found this helpful and if you’d like to find out more about Harmony please use the following links to ",e.jsx(a.a,{href:"https://open.harmony.one/",children:"Join our community"})," , ",e.jsx(a.a,{href:"https://docs.harmony.one/home/",children:"read our docs"})," and ",e.jsx(a.a,{href:"https://github.com/harmony-one",children:"contribute to our protocol"}),"."]}),`
+`,e.jsx(a.strong,{children:"Thanks for your interest in Harmony."}),`
+`,e.jsx("span",{className:"image main",children:e.jsxs(a.button,{type:"button",className:"jincubator-image-zoom","aria-label":"Open full-size image: Harmony community closing slide",children:[e.jsx("img",{src:"/posts/2019-10-16-devcon5/9_harmony.webp",alt:"Harmony community closing slide",className:"jincubator-image-zoom__image"}),e.jsx(a.span,{className:"jincubator-image-zoom__indicator","aria-hidden":"true"})]})})]})}function r(n={}){const{wrapper:a}={...s(),...n.components};return a?e.jsx(a,{...n,children:e.jsx(i,{...n})}):i(n)}export{r as default,t as frontmatter};
